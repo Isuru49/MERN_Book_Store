4dafbbadab250ffb4d21633c451a87e42e2206a5
Isuru49 <114619691+Isuru49@users.noreply.github.com>
2023-12-08 23:52:40 +0530

    Updated

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://isuru30sandeepa:kfQm40mHpxhy6GSW@books-store-mern.wm7zmtk.mongodb.net/books-collection?retryWrites=true&w=majority'
