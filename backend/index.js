7cf2a446fd23aaefb1105aca395744145ee433c9
Isuru49 <114619691+Isuru49@users.noreply.github.com>
2023-12-10 02:30:33 +0530

    Last Updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-import express, { request } from "express";
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+//import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 
@@ -10,17 +10,18 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
+app.use(cors());
 
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request,response) => {
-    console.log(request)
+    console.log(request);
     return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
